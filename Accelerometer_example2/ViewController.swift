d7791a5cd4748d46f6d89ff1925798cbe7a12629
soufuru <soufuru1@gmail.com>
2017-03-09 14:33:34 +0900

    Initial Commit

---
+++ Accelerometer_example2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accelerometer_example2
+//
+//  Created by SoichiFurukawa on 2017/03/09.
+//  Copyright © 2017年 soufuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
